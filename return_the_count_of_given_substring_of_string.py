805194d91c623975ee094623055d15ee88660c43
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-22 19:20:19 +0800

    Pseudocodes

--- return_the_count_of_given_substring_of_string.py
+++ return_the_count_of_given_substring_of_string.py
@@ -1,2 +1,16 @@
 # Exercise 7: Return the count of a given substring from a string
 # Write a program to find how many times substring “Emma” appears in the given string.
+
+# Given:
+
+# str_x = "Emma is good developer. Emma is a writer"
+
+# Expected Output:
+
+# Emma appeared 2 times
+
+#Give the string according to the given
+
+#Count how many times a substring chosen has appeared in the given
+
+#Display the output
